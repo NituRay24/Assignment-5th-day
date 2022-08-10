e91c581016796b6163630bd39aea8b8d5bf3ef5e
NituRay24 <raynitu24@gmail.com>
2022-08-10 23:32:55 +0530

    Basic core programs

---
+++ src/EvenandOdd.java
@@ -0,0 +1,11 @@
+import java.util.Scanner.*;
+public class EvenandOdd {
+    public static void main(String args[])
+    {
+        int num=11;
+        if ( num % 2 == 0 )
+            System.out.println("Entered number is even");
+        else
+            System.out.println("Entered number is odd");
+    }
+}
---
+++ src/Vowelsprogram.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Vowelsprogram {
+    public static void main(String[] args) {
+
+        char ch = 'b';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
